1e2abf698346fdc59a0728467b3d0597ee98e963
FilipeBorges1993 <sonicidentification@gamil.com>
2020-11-20 19:11:27 +0000

    first

---
+++ app.js
@@ -0,0 +1,82 @@
+var data = [];
+var m = 1;
+var b = 0;
+
+function setup(){
+	createCanvas(400, 400);
+}
+
+function linearRegression(){
+
+	var xsum = 0;
+	var ysum = 0;
+
+	for(let i = 0; i < data.length; i++){
+		xsum += data[i].x;
+		ysum += data[i].y;
+	}
+
+	var xmean = xsum / data.length; // Media
+	var ymean = ysum / data.length; // Media
+
+	var num = 0; //-> Nominator  
+	var den = 0; //-> Denominator 
+
+	for(let i = 0; i < data.length; i++){
+
+		var x = data[i].x;
+		var y = data[i].y;
+		num += (x - xmean) * (y - ymean);
+		den += (x - xmean) * (x - xmean);
+	}
+
+	m = num / den;
+	b = ymean - m * xmean;
+
+}
+
+
+function drawLine(){
+	var x1 = 0;
+	var x2 = 1;
+	var y1 = m * x1 + b;
+	var y2 = m * x2 + b;
+
+	x1 = map(x1, 0, 1, 0, width);
+	y1 = map(y1, 0, 1, height, 0);
+	x2 = map(x2, 0, 1, 0, width);
+	y2 = map(y2, 0, 1, height, 0);
+
+	stroke(255, 0, 255);
+	line(x1, y1, x2, y2);
+}
+
+
+function mousePressed(){
+	var x = map(mouseX, 0, width, 0, 1);
+	var y = map(mouseY, 0, height, 1, 0);
+	var point = createVector(x, y);
+
+	data.push(point);
+}
+
+
+function draw(){
+
+	background(51);
+
+	for(let i = 0; i < data.length; i++){
+		let x = map(data[i].x ,0, 1, 0, width);
+		let y = map(data[i].y ,0, 1, height, 0);
+
+		fill(255);
+		stroke(255);
+		ellipse(x,y,8,8);
+	}
+
+	if(data.length > 1 ){
+		linearRegression();
+		drawLine();
+	}
+
+}
